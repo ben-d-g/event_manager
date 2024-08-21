260811933298d863186dbd0fb1a1696eeeeae32f
Ben <ben.d.godfrey2002@gmail.com>
2024-08-21 20:01:01 +0100

    Iteration 2

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,20 @@
+require("csv")
 puts('Event Manager Initialized!')
+
+def clean_zipcode(zipcode)
+  return zipcode.to_s.rjust(5, "0").slice(0,5)
+end
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name, zipcode = row[:first_name], row[:zipcode]
+
+  zipcode = clean_zipcode(zipcode)
+
+  puts("#{name}, #{zipcode}")
+end
